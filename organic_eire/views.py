aab97fbba58491b46903b45bf1fbafefc0dc859f
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-19 13:02:59 +0000

    add static and css files

--- organic_eire/views.py
+++ organic_eire/views.py
@@ -6,5 +6,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "organiceire/index.html"
-    paginate_by = 6
+    template_name = "organic_eire/index.html"
+    paginate_by = 12
