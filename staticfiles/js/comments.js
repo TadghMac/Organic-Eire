a682aec75b5f5c2b3a6a08abaff85a07bcb02c5d
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-23 13:46:42 +0000

    install cloudinary, add image to posts

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -7,6 +7,7 @@ const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
 const deleteConfirm = document.getElementById("deleteConfirm");
 
+
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
     let commentId = e.target.getAttribute("comment_id");
