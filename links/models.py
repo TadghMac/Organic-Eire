a5e1e5a2d39fd8e6f2403545e04631362f086b0c
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-24 12:58:52 +0000

    fix : images not loading in links model.

--- links/models.py
+++ links/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-# Create your models here.
+
 class Links(models.Model):
     title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
     link_image = CloudinaryField('image', default='placeholder')
     link_url = models.URLField(max_length=200)
     description = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.title
