8e748c867453e8b2f1c9db48bb54be684f41e6b4
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-24 11:45:54 +0000

    links: add custom model for links ,links.html change

--- links/models.py
+++ links/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Links(models.Model):
     title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
-
+    link_image = CloudinaryField('image', default='placeholder')
+    link_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
     def __str__(self):
         return self.title
--- links/views.py
+++ links/views.py
@@ -6,7 +6,7 @@ def links_me(request):
     """
     Renders the Links page
     """
-    links = Links.objects.all().order_by('-updated_on').first()
+    links = Links.objects.all()
 
     return render(
         request,
